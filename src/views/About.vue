f0b9d7cc5dcb85476ddd1e1a1483cb69695c9a6a
Brian Wieder <brian@4wieders.com>
2020-11-11 21:22:55 -0500

    Started working on about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,94 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <link rel="stylesheet" type="text/css" href="/about.css" />
+    <div class="card">
+      <h1>Project Description and Objectives</h1>
+      <p>
+        A nonsense word, unlike a sememe, may have no definition. Nonsense words
+        can be classified depending on their orthographic and phonetic
+        similarity with (meaningful) words.[1] If it can be pronounced according
+        to a language's phonotactics, it is a pseudoword.[2] Nonsense words are
+        used in literature for poetic or humorous effect. Proper names of real
+        or fictional entities are sometimes nonsense words. Nonsense words are
+        also used by researchers and educators as a tool to assess a learner's
+        phonetic decoding ability. The words follow phonetic rules but have no
+        meaning. [3]A stunt word is a nonsense word used for a special effect,
+        or to attract attention, as part of a performance. Such words are a
+        feature of the work of Dr. Seuss ("Sometimes I am quite certain there's
+        a Jertain in the curtain").[4] The ability to infer the (hypothetical)
+        meaning of a nonsense word from context is used to test for brain
+        damage.[5]
+      </p>
+    </div>
+    <div class="card">
+      <h1>The Team</h1>
+      <div class="row">
+        <div class="aboutCard col-md-3">
+          <div class="aboutCardPadding">
+            <img src="images/joe.png" />
+            <p class="description">
+              Joe is a Mathematics Major at Virginia Tech. For this project he
+              handled code development for the simulation and a large chunk of
+              the UI. Additionally he handled most bureaucratic matters as the
+              project's PI.
+            </p>
+            <div class="row social-row">
+              <div class="col">
+                <div class="social-link">
+                  <a href="http://conwell.tech/"><i class="far fa-file"></i></a>
+                </div>
+              </div>
+              <div class="col">
+                <div class="social-link">
+                  <a href="https://www.linkedin.com/in/joe-conwell/"
+                    ><i class="fab fa-linkedin"></i
+                  ></a>
+                </div>
+              </div>
+              <div class="col">
+                <div class="social-link">
+                  <a href="https://github.com/jmc529"
+                    ><i class="fab fa-github-square"></i
+                  ></a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="aboutCard col-md-3">
+          <div class="aboutCardPadding">
+            <img src="images/ross.png" />
+            <p class="description">
+              Ross is a Creative Technologies Major at Virginia tech. For this
+              project he designed the graphic elements for the UI as well as
+              helping implement the graphing asset.
+            </p>
+            <div class="row social-row">
+              <div class="col" />
+              <div class="col">
+                <div class="social-link">
+                  <a href="https://rossw3.myportfolio.com/"
+                    ><i class="far fa-file"></i
+                  ></a>
+                </div>
+              </div>
+              <div class="col" />
+            </div>
+          </div>
+        </div>
+        <div class="aboutCard col-md-3">
+          <div class="aboutCardPadding">
+            <img src="images/joe.png" />
+            <p>Testing testing 123</p>
+          </div>
+        </div>
+        <div class="aboutCard col-md-3">
+          <div class="aboutCardPadding">
+            <img src="images/joe.png" />
+            <p>Testing testing 123</p>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
